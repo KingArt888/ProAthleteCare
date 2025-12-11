64449f059d1ef8d7a2af174fb961254392eaf8e6
KingArt888 <kulyk015@gmail.com>
2025-12-11 23:09:51 +0100

    Update daily-individual.js

--- daily-individual.js
+++ daily-individual.js
@@ -308,23 +308,4 @@ function loadAndDisplayDailyPlan() {
 }
 
 
-/**
- * Логіка для перемикання бічної панелі на мобільних пристроях
- */
-function setupMenuToggle() {
-    const toggleButton = document.getElementById('menu-toggle-button');
-    const sidebar = document.getElementById('main-sidebar');
 
-    if (toggleButton && sidebar) {
-        toggleButton.addEventListener('click', () => {
-            sidebar.classList.toggle('active');
-        });
-    }
-}
-
-
-// Запуск при завантаженні сторінки
-document.addEventListener('DOMContentLoaded', () => {
-    loadAndDisplayDailyPlan();
-    setupMenuToggle(); 
-});
